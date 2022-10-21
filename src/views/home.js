acfe9ca25de7d12368cc7ac60035f99141c9be16
Daniel Eisenberg <89800032+StolenDad2@users.noreply.github.com>
2022-10-21 23:15:08 +0300

    Commit from TeleportHQ

--- src/views/home.js
+++ src/views/home.js
@@ -11,7 +11,11 @@ const Home = (props) => {
         <title>Taqueria Calculator</title>
         <meta property="og:title" content="Taqueria Calculator" />
       </Helmet>
-      <h1 className="home-text">?שמנת כוסברה חריף סבבה</h1>
+      <button className="button">?שמנת, כוסברה, חריף סבבה</button>
+      <img
+        alt="pastedImage"
+        src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/29d4a971-6ec8-423c-89b4-41db65e59d85/e3368f47-ede4-40ae-a795-54e542722da0?org_if_sml=14155414"
+      />
     </div>
   )
 }
