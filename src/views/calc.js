4ff9e7c0a7dd9c97be6ac41bbddf44623f908c17
Daniel Eisenberg <89800032+StolenDad2@users.noreply.github.com>
2022-10-22 13:01:22 +0300

    Update calc.js

--- src/views/calc.js
+++ src/views/calc.js
@@ -5,14 +5,14 @@ import { Helmet } from 'react-helmet'
 import './calc.css'
 
 const Calc = (props) => {
-  return (
+  return (`
     <div className="calc-container">
       <Helmet>
         <title>calc - Taqueria Calculator</title>
         <meta property="og:title" content="calc - Taqueria Calculator" />
       </Helmet>
       <div className="calc-container1"></div>
-    </div>
+    </div>`
   )
 }
 
